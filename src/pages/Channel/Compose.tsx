f4c92c71eff72b44eaf70684a81ad7969d7efb97
Ilham Adiputra <job.ilhamadiputra4@gmail.com>
2021-12-31 21:24:52 +0700

    chore: add all members of channel into thread tags as default

--- src/pages/Channel/Compose.tsx
+++ src/pages/Channel/Compose.tsx
@@ -91,6 +91,7 @@ function Compose() {
         name: values.name,
         content: values.content,
         channel: params.channelId,
+        tags: channelData.members,
       });
 
       deleteDraft();
